d6339e0682edc052522880fdf1eab4a007ed88c7
Ankit <guptankit.2003@gmail.com>
2024-07-21 00:37:08 +0530

    not completed

---
+++ class3.js
@@ -0,0 +1,11 @@
+getName() // Namaste JavaScript
+console.log(x) // undefined
+
+var x = 7
+
+function getName (){
+  console.log("Namaste JavaScript")
+}
+
+// getName() // Namaste JavaScript
+// console.log(x) // 7
