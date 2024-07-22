c170788d5d20779f245e9af2956bc4385423895c
Ankit <guptankit.2003@gmail.com>
2024-07-22 14:27:44 +0530

    undefined vs not

---
+++ Ep6.js
@@ -0,0 +1,25 @@
+// console.log(a)
+// var a = 7
+// console.log(a)
+
+// var a
+
+// console.log(a)
+// a = 10
+
+// if(a === undefined){
+//   console.log("a is undefined")
+// }
+// else {
+//   console.log("a is not undefined")
+// }
+
+var a
+console.log(a)
+a = 10
+console.log(a)
+a = "hello world"
+console.log(a)
+
+a = undefined
+console.log(a)
