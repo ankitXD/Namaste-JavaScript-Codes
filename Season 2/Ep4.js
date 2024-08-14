9b74b912059f11711f2b358e6ac32e71be3d4696
Ankit <guptankit.2003@gmail.com>
2024-08-14 20:28:05 +0530

    async await

---
+++ Season 2/Ep4.js
@@ -0,0 +1,11 @@
+// What is async?
+// What is await?
+// How async await works behind the scenes?
+// Example of using async/await
+// Error Handling
+// Interviews
+// Async await vs Promise.then/.catch
+
+async function getData() {
+  return "Namaste";
+}
