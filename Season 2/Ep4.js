2259386b51414ecb815a294a4cf37787e9893005
Ankit <guptankit.2003@gmail.com>
2024-08-15 19:00:52 +0530

    updated

--- Season 2/Ep4.js
+++ Season 2/Ep4.js
@@ -6,6 +6,28 @@
 // Interviews
 // Async await vs Promise.then/.catch
 
-async function getData() {
-  return "Namaste";
-}
+// async function getData() {
+//   return p
+// }
+
+// const dataPromise = getData()
+
+// dataPromise.then((res)=> console.log(res))
+
+// const p = new Promise((resolve,reject)=>{
+//   resolve("Promise Resolved Value")
+// })
+
+// async function handlePromise() {
+//   const val = await p;
+//   console.log(val);
+// }
+// handlePromise();
+
+async function handlePromise() {
+  // fetch() => Response Object which as body as Readable stream => Response.json() is also a promise which when resolved => value
+  const data = await fetch('https://api.github.com/users/ankitXD');
+  const res = await data.json();
+  console.log(res);
+};
+handlePromise()
